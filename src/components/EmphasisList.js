f69be711b82e525f37fc37123ec429a5016e1d82
EduuCarvalho <educarvalho.engcivil@gmail.com>
2022-11-27 22:26:14 -0300

    feat: scroll catalogList and emphasisList

--- src/components/EmphasisList.js
+++ src/components/EmphasisList.js
@@ -24,6 +24,17 @@ const GameList = styled.ul`
     gap: 15px;
 
     &::-webkit-scrollbar {
-        display: none;
+        width: 15px;
+        padding: 5px;
+    }
+
+    &::-webkit-scrollbar-track {
+        background: none;
+    }
+
+    &::-webkit-scrollbar-thumb {
+        background-color: #ffffff;
+        border-radius: 10px;
+        border: 1px solid #ffffff;
     }
 `
